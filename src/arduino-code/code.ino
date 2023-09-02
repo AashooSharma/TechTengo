e6bac14db4e0cdb24b8994260a70e280c9c06539
Abhishek Sharma <115462704+myaashoolab@users.noreply.github.com>
2023-09-03 01:50:44 +0530

    Update code.ino

--- src/arduino-code/code.ino
+++ src/arduino-code/code.ino
@@ -1,1 +1,30 @@
+int redPin = 2;
+int yellowPin = 3;
+int greenPin = 4;
 
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+}
+
+void loop() {
+  // Red light
+  digitalWrite(redPin, HIGH);
+  delay(2000);  // 2 seconds
+  
+  // Yellow light
+  digitalWrite(redPin, LOW);
+  digitalWrite(yellowPin, HIGH);
+  delay(1000);  // 1 second
+  
+  // Green light
+  digitalWrite(yellowPin, LOW);
+  digitalWrite(greenPin, HIGH);
+  delay(2000);  // 2 seconds
+  
+  // Yellow light again
+  digitalWrite(greenPin, LOW);
+  digitalWrite(yellowPin, HIGH);
+  delay(1000);  // 1 second
+}
